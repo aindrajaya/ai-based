f2dae9ce1aa938ffaf7e0d2548ae5c2ae8d94de8
Arista indrajaya <arista.indrajaya@gmail.com>
2024-01-24 03:49:15 +0700

    Complete code
    complete code AI-based apps

---
+++ chat-indoagri-ai.js
@@ -0,0 +1,53 @@
+import { openai } from "./openai.js";
+import readline from "node:readline"; //call node.js internal package readline
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+const newMessage = async (history, message) => {
+  const res = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    messages: [...history, message]
+  }); //create a new chat
+
+  return res.choices[0].message; //return the message
+}
+
+const formatMessage = (userInput) => ({
+  role: "user",
+  content: userInput
+})
+
+const chat = () => {
+  const history = [
+    {
+      role: "system",
+      content: "You are an AI Assistant. Just answer the user's questions only about agriculture using Indonesian language."
+    }
+  ]
+  
+  const start = () => {
+    rl.question("Human: ", async(userInput) => {
+      if(userInput.toLowerCase() === "exit") {
+        rl.close()
+        return
+      }
+
+      const userMessage = formatMessage(userInput);
+      const response = await newMessage(history, userMessage);
+
+      history.push(userMessage, response);
+      console.log(`\n\nAI Assistant: ${response.content}\n\n`);
+      start();
+    })
+  }
+
+  start();
+  console.log("AI Assistant: How can I help you today?\n\n");
+}
+
+console.log("Chatbot Initialized. Type 'exit' to quit");
+chat();
+
